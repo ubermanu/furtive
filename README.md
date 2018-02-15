c55e911f70cae6b65ce6b9804ffe7e0e6ea57891
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2018-02-15 17:25:35 +0100

    [TASK] First commit with example

---
+++ README.md
@@ -0,0 +1,37 @@
+# jQuery - Reveal
+
+A plugin that toggles visibility of HTML elements according to Form input values.
+
+#### Getting started
+
+Once you've included **reveal.js** and **jQuery** to your page, you can attach the Reveal watcher to a container.<br>
+All the form elements (input, select, textarea etc...) will trigger the watcher handler.<br>
+
+```html
+<div data-reveal-watch>
+```
+
+#### Conditions
+
+Then, in your container you can attach some conditions to other elements.<br>
+The conditions are simple jQuery selectors.<br>
+If one of the selector matches, the condition is fulfilled and the element is displayed.<br>
+**Note:** The selectors must be in the main watcher scope, see above.
+
+```html
+<div data-reveal-condition="#input:checked">
+```
+> This example will show the &lt;div&gt; element if #input is checked
+
+
+#### Conjunction
+
+Sometimes it's necessary to have multiple conditions that MUST match.<br>
+It's possible to define the conjunction method between conditions.<br>
+The **OR** conjunction is used by default.
+
+```html
+<div data-reveal-condition="#input:checked, #input2:checked"
+     data-reveal-conjunction="and">
+```
+> This example will show the &lt;div&gt; element if #input is checked and #input2 is checked too.
---
+++ src/reveal.js
@@ -0,0 +1,98 @@
+;(function (window) {
+
+    /**
+     * This function will bind all the main watcher to all the container elements.
+     * TODO: Add "disabled" property for hidden input elements
+     *
+     * @constructor
+     */
+    var Reveal = function ($container) {
+
+        var condition = 'data-reveal-condition';
+        var conjunction = 'data-reveal-conjunction';
+
+        // Use an internal copy of jQuery at this point (to avoid conflict with other pseudos)
+        var _jQuery = jQuery;
+        _jQuery.extend(jQuery.expr[':'], Reveal.pseudos);
+
+        var watcher = function () {
+            $container.find('[' + condition + ']').each(function (k, item) {
+
+                var cond = _jQuery(item).attr(condition);
+                var conj = _jQuery(item).attr(conjunction);
+
+                if (cond !== undefined) {
+
+                    // Strip contained quoted strings
+                    var condCount = cond.replace(/'((?:\\.|[^'\\])*)'/g, '').split(',').length;
+
+                    var visible = (conj && conj.toLowerCase() === 'and')
+                        ? $container.find(cond).length === condCount
+                        : $container.find(cond).length > 0;
+
+                    $container.find(item).toggle(visible);
+                }
+            });
+        };
+
+        $container
+            .find(':checkbox, :radio, :file, select')
+            .change(watcher)
+            .triggerHandler('change');
+
+        $container
+            .find(':text, textarea')
+            .keyup(watcher)
+            .triggerHandler('keyup');
+    };
+
+    /**
+     * Initialize the Reveal plugin on the selection.
+     *
+     * @returns {Array}
+     * @constructor
+     */
+    $.fn.reveal = function () {
+
+        if (this.length > 1) {
+            this.each(function () {
+                $(this).reveal();
+            });
+        }
+
+        if (this.length === 1) {
+            Reveal($(this));
+        }
+    };
+
+    /**
+     * Ensure the extension is now defined if it wasn't previously.
+     * Auto binding is set to TRUE by default.
+     * Custom "pseudos" can be added as well.
+     */
+    if ('undefined' === typeof window.Reveal) {
+        window.Reveal = {
+            'autoBind': true,
+            'pseudos': {
+                'equals': function (e, index, parts) {
+                    return $(e).val() === parts[3];
+                },
+                'filled': function (e) {
+                    return $(e).val().length > 0;
+                }
+            }
+        };
+    }
+
+    /**
+     * Auto-binding can be prevented by settings the "window.Reveal.autoBind" value to FALSE.
+     */
+    if (false !== window.Reveal.autoBind) {
+        $(function () {
+            if ($('[data-reveal-watch]').length) {
+                $('[data-reveal-watch]').reveal();
+            }
+        });
+    }
+
+}(window));

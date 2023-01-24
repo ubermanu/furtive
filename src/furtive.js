75fdf67bad3e95b00e8063db66c236f719410694
ubermanu <manu@paca.happy-dev.fr>
2023-01-24 16:27:24 +0100

    feat: use a custom data-attribute to show/hide the elements

--- src/furtive.js
+++ src/furtive.js
@@ -4,77 +4,95 @@ const condition = 'data-furtive-condition'
 const conjunction = 'data-furtive-conjunction'
 const disabled = 'data-furtive-disabled'
 const required = 'data-furtive-required'
+const hidden = 'data-furtive-hidden'
 
 /**
- * This function will bind all the main watcher to all the container elements.
- *
+ * When a change is detected into the sub-elements, the watcher will be triggered.
+ * Re-evaluate the condition and show/hide the element accordingly.
  * @private
  */
-let Furtive = function ($container) {
+const update = ($container) => () => {
   // Use an internal copy of jQuery at this point (to avoid conflict with other pseudos)
   const _jQuery = $
   _jQuery.extend($.expr[':'], window.Furtive.pseudos)
 
-  let watcher = function () {
-    $container.find('[' + condition + ']').each(function (k, item) {
-      let cond = $(item).attr(condition)
-      let conj = $(item).attr(conjunction)
-      let $item = $(item)
+  $container.find('[' + condition + ']').each((k, item) => {
+    let cond = $(item).attr(condition)
+    let conj = $(item).attr(conjunction)
+    let $item = $(item)
 
-      if (cond !== undefined && cond.length) {
-        // Extract selectors from the given condition
-        let selectors = cond.match(/('.*?'|[^',]+)+(?=\s*|\s*$)/g)
-        let matches = 0
+    // If the condition is empty, show the element
+    if (cond === undefined || !cond.length) {
+      $item.attr(hidden, false)
+      return
+    }
 
-        for (let i = 0, l = selectors.length; i < l; i++) {
-          if (_jQuery(selectors[i]).length) {
-            matches++
+    // Extract selectors from the given condition
+    let selectors = cond.match(/('.*?'|[^',]+)+(?=\s*|\s*$)/g)
+    let matches = 0
+
+    for (let i = 0, l = selectors.length; i < l; i++) {
+      if (_jQuery(selectors[i]).length) {
+        matches++
+      }
+    }
+
+    // Resolve the visibility of the element according to the conjunction
+    // and the number of matches from the condition
+    const visible =
+      conj && conj.toLowerCase() === 'and'
+        ? matches === selectors.length
+        : matches > 0
+
+    // Set the custom "hidden" attribute to the element
+    $item.attr(hidden, !visible)
+
+    // Disable/enable the hidden <input> elements
+    // This is useful to prevent the browser to validate the form with hidden fields
+    if (window.Furtive.disableHidden) {
+      $item.find(':input').each((z, input) => {
+        let $input = $(input)
+
+        // Stash the old disabled and require properties and apply new one
+        if (!visible) {
+          if ($input.attr(disabled) === undefined) {
+            $input.attr(disabled, $input.prop('disabled'))
+            $input.prop('disabled', true)
+          }
+
+          if ($input.attr(required) === undefined) {
+            $input.attr(required, $input.prop('required'))
+            $input.prop('required', false)
           }
         }
 
-        let visible =
-          conj && conj.toLowerCase() === 'and'
-            ? matches === selectors.length
-            : matches > 0
+        // Re-apply the old disabled and required properties
+        if (visible) {
+          $input.prop('disabled', $input.attr(disabled) === 'true')
+          $input.removeAttr(disabled)
 
-        $item.toggle(visible)
-
-        if (false !== window.Furtive.disableHidden) {
-          $item.find(':input').each(function (z, input) {
-            let $input = $(input)
-
-            // Stash the old disabled and require properties and apply new one
-            if (!visible) {
-              if ($input.attr(disabled) === undefined) {
-                $input.attr(disabled, $input.prop('disabled'))
-                $input.prop('disabled', true)
-              }
-
-              if ($input.attr(required) === undefined) {
-                $input.attr(required, $input.prop('required'))
-                $input.prop('required', false)
-              }
-            }
-
-            // Re-apply the old disabled and required properties
-            if (visible) {
-              $input.prop('disabled', $input.attr(disabled) === 'true')
-              $input.removeAttr(disabled)
-
-              $input.prop('required', $input.attr(required) === 'true')
-              $input.removeAttr(required)
-            }
-          })
+          $input.prop('required', $input.attr(required) === 'true')
+          $input.removeAttr(required)
         }
-      }
-    })
-  }
+      })
+    }
+  })
+}
 
+/**
+ * Return the start function.
+ * This function will bind all the main watcher to all the container elements.
+ *
+ * @param {jQuery} $container
+ */
+export default ($container) => {
   $container
     .find(':checkbox, :radio, :file, select')
-    .change(watcher)
+    .on('change', update($container))
     .triggerHandler('change')
 
-  $container.find(':text, textarea').keyup(watcher).triggerHandler('keyup')
+  $container
+    .find(':text, textarea')
+    .on('keyup', update($container))
+    .triggerHandler('keyup')
 }
-export default Furtive

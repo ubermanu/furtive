1ea352052250f84a97a7860dc53d967a6394dc71
ubermanu <manu@paca.happy-dev.fr>
2023-01-25 14:37:11 +0100

    update exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,32 +3,30 @@ import terser from '@rollup/plugin-terser'
 import copy from 'rollup-plugin-copy'
 import pkg from './package.json' assert { type: 'json' }
 
-const output = {
-  file: pkg.main,
-  format: 'umd',
-  sourcemap: true,
-  globals: {
-    jquery: '$',
-  },
-}
-
 export default [
   {
     input: 'src/plugin.js',
     output: [
-      output,
       {
-        ...output,
-        file: pkg.main.replace(/.js$/, '.min.js'),
-        plugins: [terser()],
+        file: pkg.unpkg,
+        format: 'umd',
+        sourcemap: false,
+        globals: {
+          jquery: '$',
+        },
       },
     ],
-    plugins: [json()],
+    plugins: [json(), terser()],
     external: ['jquery'],
   },
   {
     input: 'src/furtive.js',
     output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
       {
         file: pkg.module,
         format: 'es',
